cde4845c6fbc69337bf3b688785bb112e2734001
jeevash341 <jeevashkhatri676@gmail.com>
2025-10-18 18:06:49 +0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+});
+
+
+
